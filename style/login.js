122d8a9e638d02198f76ca972a5169240b3a3fd3
mehedy215781 <mehedy215781@gmail.com>
2026-03-07 01:33:26 +0600

    sign in to dash board and dashbord navbar design

--- style/login.js
+++ style/login.js
@@ -1,3 +1,16 @@
 document.getElementById("signin-btn").addEventListener("click",function(){
-    console.log("login-button");
+    const userNameInput =document.getElementById("user-name");
+    const userName = userNameInput.value;
+    // console.log(userName);
+    const  passwordInput = document.getElementById("password");
+    const password = passwordInput.value;
+    // console.log(password);
+    if(userName === "admin" && password === "admin123" ){
+        alert('sign in Succesfull');
+        window.location.assign("./dashbord.html");
+    }else{
+        alert("sign in Failed");
+        return;
+    }
+
 })
